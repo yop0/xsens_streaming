ff6ee4ef7359e5395127ee243329faf69e93459f
Arnaud Tanguy <arn.tanguy@gmail.com>
2021-12-09 12:34:25 +0100

    [ci/clang-format] Clang-format code

--- src/streaming_protocol/main.cpp
+++ src/streaming_protocol/main.cpp
@@ -1,36 +1,36 @@
 /*! \file
-	\section FileCopyright Copyright Notice
-	This is free and unencumbered software released into the public domain.
+  \section FileCopyright Copyright Notice
+  This is free and unencumbered software released into the public domain.
 
-	Anyone is free to copy, modify, publish, use, compile, sell, or
-	distribute this software, either in source code form or as a compiled
-	binary, for any purpose, commercial or non-commercial, and by any
-	means.
+  Anyone is free to copy, modify, publish, use, compile, sell, or
+  distribute this software, either in source code form or as a compiled
+  binary, for any purpose, commercial or non-commercial, and by any
+  means.
 
-	In jurisdictions that recognize copyright laws, the author or authors
-	of this software dedicate any and all copyright interest in the
-	software to the public domain. We make this dedication for the benefit
-	of the public at large and to the detriment of our heirs and
-	successors. We intend this dedication to be an overt act of
-	relinquishment in perpetuity of all present and future rights to this
-	software under copyright law.
+  In jurisdictions that recognize copyright laws, the author or authors
+  of this software dedicate any and all copyright interest in the
+  software to the public domain. We make this dedication for the benefit
+  of the public at large and to the detriment of our heirs and
+  successors. We intend this dedication to be an overt act of
+  relinquishment in perpetuity of all present and future rights to this
+  software under copyright law.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-	OTHER DEALINGS IN THE SOFTWARE.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include "udpserver.h"
-#include "streamer.h"
-#include <xsens/xstime.h>
 #include <boost/program_options.hpp>
+#include "streamer.h"
+#include "udpserver.h"
+#include <xsens/xstime.h>
 namespace po = boost::program_options;
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
   auto host = std::string{};
   auto port = 0u;
@@ -51,10 +51,9 @@ int main(int argc, char *argv[])
     return 1;
   }
 
-	UdpServer udpServer(host, (uint16_t)port);
+  UdpServer udpServer(host, (uint16_t)port);
 
-	while(true)
-		XsTime::msleep(10);
+  while(true) XsTime::msleep(10);
 
-	return 0;
+  return 0;
 }
